4145ac80c9066d99f98a3e6036c61e65db2b7cd6
kanxinwei <shink_7770@qq.com>
2016-07-30 15:33:21 +0800

    增加user模型及验证

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/hello', to: 'hello#hello'
-      resources :accounts
       resources :microposts
     end
   end
