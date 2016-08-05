c72879d61d492ee06fdb3ba8bc926858830b8780
kanxinwei <shink_7770@qq.com>
2016-08-05 18:04:07 +0800

    添加解析带jwt的header的方法

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::API
+  attr_accessor :current_user
+
   def get_token_by_user(user)
     exp = Time.now.to_i + 7 * 24 * 3600#到期时间
     iat = Time.now.to_i#发行时间
-    payload = { :user_id => user.id, :exp => exp, :iat => iat }
+    payload = { :id => user.id, :exp => exp, :iat => iat }
     JWT.encode(payload, Jwt_hmac_secret, 'HS256')
   end
 
@@ -15,5 +17,20 @@ class ApplicationController < ActionController::API
       return nil
     end
     User.find(decoded_token[0]['user_id'])
+  end
+
+  def authenticate_jwt
+    token, options = ActionController::HttpAuthentication::Token.token_and_options(request)
+    return unauthorize_error if token.nil?
+    user = get_user_by_token(token)
+    if user
+      self.current_user = user
+    else
+      return unauthorize_error
+    end
+  end
+
+  def unauthorize_error
+    render plain: "用户未授权", status: :unauthorized
   end
 end
